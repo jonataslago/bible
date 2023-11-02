a609bc467597892cc9717d93ff3500b14dc0ffad
Jônatas Lago <89043103+jonataslago@users.noreply.github.com>
2023-11-02 10:25:01 -0300

    Create and Configurate the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ChapterComponent } from './components/chapter/chapter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    HeaderComponent,
+    HomeComponent,
+    ChapterComponent
   ],
   imports: [
     BrowserModule,
